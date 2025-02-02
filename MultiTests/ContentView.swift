93ba7257a4e19f1e7e5d7ac769ea7894350de1ae
Niels Ouvrard <91876336+NielsOuvrard@users.noreply.github.com>
2025-02-02 15:39:26 -0600

    works wierframe

--- MultiTests/ContentView.swift
+++ MultiTests/ContentView.swift
@@ -5,20 +5,127 @@
 //  Created by Niels Ouvrard on 02/02/2025.
 //
 
+import MetalKit
+import ModelIO
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+struct MetalView: NSViewRepresentable {
+    let device: MTLDevice!
+    let commandQueue: MTLCommandQueue!
+    let pipelineState: MTLRenderPipelineState!
+    let mesh: MTKMesh!
+
+    init() {
+        device = MTLCreateSystemDefaultDevice()!
+        
+        let frame = CGRect(x: 0, y: 0, width: 600, height: 600)
+        let view = MTKView(frame: frame, device: device)
+        view.clearColor = MTLClearColor(red: 1, green: 1, blue: 0.8, alpha: 1)
+
+        let allocator = MTKMeshBufferAllocator(device: device)
+//        let mdlMesh = MDLMesh(
+//            sphereWithExtent: [0.75, 0.75, 0.75],
+//            segments: [100, 100],
+//            inwardNormals: false,
+//            geometryType: .triangles,
+//            allocator: allocator)
+        
+        let mdlMesh = MDLMesh(
+          coneWithExtent: [1, 1, 1],
+          segments: [10, 10],
+          inwardNormals: false,
+          cap: true,
+          geometryType: .triangles,
+          allocator: allocator)
+
+        
+        
+        mesh = try! MTKMesh(mesh: mdlMesh, device: device)
+      
+        
+        commandQueue = device.makeCommandQueue()!
+        
+        
+        guard let library = device.makeDefaultLibrary() else {
+            fatalError("No .metal files in the Xcode project")
         }
-        .padding()
+
+        let vertexFunction = library.makeFunction(name: "vertex_main")!
+        let fragmentFunction = library.makeFunction(name: "fragment_main")!
+        
+        let pipelineDescriptor = MTLRenderPipelineDescriptor()
+        pipelineDescriptor.vertexFunction = vertexFunction
+        pipelineDescriptor.fragmentFunction = fragmentFunction
+        pipelineDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+        
+        pipelineDescriptor.vertexDescriptor = MTKMetalVertexDescriptorFromModelIO(mesh.vertexDescriptor)
+
+        pipelineState = try! device.makeRenderPipelineState(descriptor: pipelineDescriptor)
+    }
+
+    func makeNSView(context: Context) -> MTKView {
+        let view = MTKView()
+        view.device = device
+        view.delegate = context.coordinator
+        view.clearColor = MTLClearColor(red: 0, green: 0, blue: 0, alpha: 1)
+        return view
+    }
+
+    func updateNSView(_ nsView: MTKView, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, MTKViewDelegate {
+        let parent: MetalView
+
+        init(_ parent: MetalView) {
+            self.parent = parent
+            super.init()
+        }
+
+        func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {}
+
+        func draw(in view: MTKView) {
+            guard let drawable = view.currentDrawable else { return }
+
+            let renderPassDescriptor = MTLRenderPassDescriptor()
+            renderPassDescriptor.colorAttachments[0].texture = drawable.texture
+            renderPassDescriptor.colorAttachments[0].loadAction = .clear
+            renderPassDescriptor.colorAttachments[0].storeAction = .store
+            renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(
+                red: 0, green: 0, blue: 0, alpha: 1)
+
+            let commandBuffer = parent.commandQueue.makeCommandBuffer()!
+            let renderEncoder = commandBuffer.makeRenderCommandEncoder(
+                descriptor: renderPassDescriptor)!
+
+            renderEncoder.setRenderPipelineState(parent.pipelineState)
+
+            renderEncoder.setVertexBuffer(parent.mesh.vertexBuffers[0].buffer, offset: 0, index: 0)
+            renderEncoder.setTriangleFillMode(.lines)
+            
+            guard let submesh = parent.mesh.submeshes.first else {
+              fatalError()
+            }
+            renderEncoder.drawIndexedPrimitives(
+              type: .triangle,
+              indexCount: submesh.indexCount,
+              indexType: submesh.indexType,
+              indexBuffer: submesh.indexBuffer.buffer,
+              indexBufferOffset: 0)
+
+            renderEncoder.endEncoding()
+            commandBuffer.present(drawable)
+            commandBuffer.commit()
+        }
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView: View {
+    var body: some View {
+        MetalView()
+            .frame(width: 600, height: 600)
+    }
 }
---
+++ MultiTests/Shaders.metal
@@ -0,0 +1,22 @@
+//
+//  shader.metal
+//  MultiTests
+//
+//  Created by Niels Ouvrard on 02/02/2025.
+//
+
+
+#include <metal_stdlib>
+using namespace metal;
+
+struct VertexIn {
+    float4 position [[attribute(0)]];
+};
+
+vertex float4 vertex_main(const VertexIn vertex_in [[stage_in]]) {
+    return vertex_in.position;
+}
+
+fragment float4 fragment_main() {
+    return float4(0, 0.4, 0.21, 1);
+}
