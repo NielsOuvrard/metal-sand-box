0f2ced5923bae17134e333685d488e508e1efe5e
Niels Ouvrard <91876336+NielsOuvrard@users.noreply.github.com>
2025-02-02 14:33:32 -0600

    Initial Commit

---
+++ MultiTests/MultiTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiTestsApp.swift
+//  MultiTests
+//
+//  Created by Niels Ouvrard on 02/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct MultiTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MultiTestsTests/MultiTestsTests.swift
@@ -0,0 +1,16 @@
+//
+//  MultiTestsTests.swift
+//  MultiTestsTests
+//
+//  Created by Niels Ouvrard on 02/02/2025.
+//
+
+import Testing
+
+struct MultiTestsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
